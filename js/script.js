eef7dd9dc23a5f047547a60faf0abab39a3cca93
JULIA APARECIDA DA SILVA FONSECA <23111808@CFP127ALUNO.LOCAL>
2024-05-21 14:22:09 -0300

    IA em manutenção

--- js/script.js
+++ js/script.js
@@ -1,49 +1,61 @@
-const button = document.getElementById('send--question')
+const button = document.getElementById('button')
 
+const consultaGemini = (question) => {
+    const keyGoogle = 'AIzaSyBZaybh57iVi23jcLvzuIrabNG4f3td60A'
+    const url = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=' + keyGoogle
 
-const consultaGemini = async (question) => {
+    const requestData = {
+        contents: [
+            {
+                parts: [
+                    {
+                        text: `${question}`
+                    }
+                ]
+            }
+        ]
+    }
 
-const keyGoogle = 'AIzaSyBZaybh57iVi23jcLvzuIrabNG4f3td60A'
-const url = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=' + keyGoogle
+    const requestOptions = {
+        method:'POST',
+        headers: {
+            'Content-Type': 'application/json' 
+        },
+        body: JSON.stringify(requestData)
+    }
 
-const requestData = {
-
-    contents: [
-        {
-            parts: [
-                {
-                    text: '${question}'
-                }
-            ]
-        }
-    ]
-
+    fetch(url, requestOptions)
+    .then(response => response.json())
+    .then(data => {
+        const responseTextIa = data.candidates[0].content.parts[0].text
+        respostaIa(responseTextIa)
+    })
+    .catch(error => console.error('Error: ', error))
 }
 
-const requestOpitions = {
-    method: 'POST',
-    headers: {
-        'Content-Type' : 'application/json'
-    },
-    body: JSON.stringify(requestData)
+const consultaMyMemory = (text, callback) => {
+    const url = `https://api.mymemory.translated.net/get?q=${encodeURIComponent(text)}&langpair=pt|en`
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        const translatedText = data.responseData.translatedText
+        callback(translatedText)
+    })
+    .catch(error => console.error('Error: ', error))
 }
 
 
-await fetch (url, requestOpitions)
-.then (response => response.json())
-.then (data => {
-    const respostaTextIA = data.candidates[0].content.parts[0].text
-    respostaTextIA(respostaIa)
-})
+const respostaIa = (responseTextIa) => {
+    const responseIaElement = document.getElementById('response-ia')
+    responseIaElement.innerText = responseTextIa
 
-const respostaIa = (respostaTextIA) => {
-
-    const textAreaPt = document.getElementById('answer-pt')
-    textAreaPt.value = respostaTextIA
+    consultaMyMemory(responseTextIa, (translatedText) => {
+        const responseEnElement = document.querySelector('.resposta2')
+        responseEnElement.innerText = translatedText
+    })
 }
 
-
-}
 
 button.addEventListener('click', () => {
     const question = document.getElementById('ask--user').value
